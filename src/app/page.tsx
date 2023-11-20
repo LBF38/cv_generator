6e8c88b4bcb087a3dddc27073b9916101e945dd5
LBF38 <contact.lbf38@gmail.com>
2023-11-20 08:51:52 +0100

    refactor(ui): :recycle: update experiences page
    improve and fix page w/ fake_data entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 import ExperienceCard, { ExperienceCardProps } from '@/components/experience-card';
 import { NavigationMenuDemo } from '@/components/navigation-bar';
-import { sections } from '@/lib/fake_data'; // TODO: replace it w/ real data. (from db or local storage)
+import { SectionsEntity, fake_sections as fakeSections } from '@/lib/fake_data'; // TODO: replace it w/ real data. (from db or local storage)
+import { useEffect, useState } from 'react';
 
 
 export default function Home() {
+  const [sections, setSections] = useState<SectionsEntity[]>([]);
+  useEffect(() => setSections(fakeSections), []);
+
   return (
     <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
       <header>
         <NavigationMenuDemo />
       </header>
       <main>
+        <h1 className='text-4xl font-bold text-center my-5'>Personal Portfolio</h1>
         {sections.map((section) => (
           <section id={section.id} key={section.id}>
-            <h1>{section.title}</h1>
+            <h1 className='text-2xl font-semibold leading-none tracking-tight'>{section.title}</h1>
             {section.experiences.map((experience, index) => (
               <ExperienceCard key={index} {...experience} />
             ))}
