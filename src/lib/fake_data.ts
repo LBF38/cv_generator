97ca5f27ef8f8f543e2346d162fede446dc56f8d
LBF38 <contact.lbf38@gmail.com>
2023-11-20 08:51:52 +0100

    refactor(ui): :recycle: update experiences page
    improve and fix page w/ fake_data entries

--- src/lib/fake_data.ts
+++ src/lib/fake_data.ts
@@ -1,20 +1,10 @@
 import { ExperienceCardProps } from "@/components/experience-card";
 import { generateExperience, getRandomInt } from "./utils";
 
-export const sections: { title: string, id: string, experiences: Partial<ExperienceCardProps>[] }[] = [
-    {
-        title: "Experiences",
-        id: "#experiences",
-        experiences: Array.from({ length: getRandomInt(2, 5) }, generateExperience)
-    },
-    {
-        title: "Projects",
-        id: "#projects",
-        experiences: Array.from({ length: getRandomInt(2, 5) }, generateExperience)
-    },
-    {
-        title: "Studies",
-        id: "#studies",
-        experiences: Array.from({ length: getRandomInt(2, 5) }, generateExperience)
-    }
+export class SectionsEntity {
+    constructor(public title: string, public id: string, public experiences: Partial<ExperienceCardProps>[]) { }
+};
+
+export const fake_sections: SectionsEntity[] = [new SectionsEntity("About", "#about", [{ title: "About", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.", content: "", footer: "" }]),
+new SectionsEntity("Experiences", "#experiences", Array.from({ length: getRandomInt(2, 5) }, generateExperience)), new SectionsEntity("Projects", "#projects", Array.from({ length: getRandomInt(2, 5) }, generateExperience)), new SectionsEntity("Studies", "#studies", Array.from({ length: getRandomInt(2, 5) }, generateExperience)),
 ];
