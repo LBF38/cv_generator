d33751c01ba7c84b903d276a0343ef24f28f0310
LBF38 <contact.lbf38@gmail.com>
2023-12-18 10:27:08 +0100

    feat(ui): :sparkles: update template design component

--- src/components/template-design.tsx
+++ src/components/template-design.tsx
@@ -1,3 +1,4 @@
+"use client";
 import type { Template } from '@pdfme/common';
 import { generate } from '@pdfme/generator';
 import { barcodes, image, text } from '@pdfme/schemas';
@@ -10,20 +11,13 @@ import {
     getTemplateFromJsonFile,
     readFile
 } from '../lib/helper';
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from './ui/menubar';
 
-const headerHeight = 60;
-const controllerHeight = 60;
-
-const TemplateDesign = () => {
+export default function TemplateDesign() {
     const designerRef = useRef<HTMLDivElement | null>(null);
     const designer = useRef<Designer | null>(null);
     const [template, setTemplate] = useState<Template>(getSampleTemplate());
-    const [smallDisplay, setSmallDisplay] = useState(true);
     const [prevDesignerRef, setPrevDesignerRef] = useState<typeof designerRef | null>(null);
-
-    useEffect(() => {
-        setSmallDisplay(window.innerWidth < 900);
-    }, []);
 
     const buildDesigner = () => {
         if (designerRef.current) {
@@ -36,6 +30,10 @@ const TemplateDesign = () => {
             designer.current.onChangeTemplate(setTemplate);
         }
     };
+
+    useEffect(() => {
+        buildDesigner();
+    })
 
     const changeBasePdf = (file: File) => {
         if (designer.current) {
@@ -52,9 +50,7 @@ const TemplateDesign = () => {
                     designer.current?.updateTemplate(t);
                 })
                 .catch((e) => {
-                    alert(`Invalid template file.
---------------------------
-${e}`);
+                    console.log(e);
                 });
         }
     };
@@ -80,144 +76,62 @@ ${e}`);
         }
         buildDesigner();
         setPrevDesignerRef(designerRef);
+        console.log('build designer', designerRef, designer);
     }
 
     return (
         <>
-            <div
-                style={{
-                    height: controllerHeight,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    padding: '0 1rem',
-                }}
-            >
-                {smallDisplay ? (
-                    <div className="dropdown dropdown--hoverable dropdown--right">
-                        <button className="button button--sm button--outline button--primary">...</button>
-                        <ul className="dropdown__menu">
-                            <li>
-                                <label style={{ display: 'flex', alignItems: 'center' }} className="dropdown__link">
-                                    Change BasePDF
-                                    <input
-                                        style={{ display: 'none' }}
-                                        type="file"
-                                        accept="application/pdf"
-                                        onChange={(e) => {
-                                            if (e.target && e.target.files) {
-                                                changeBasePdf(e.target.files[0]);
-                                            }
-                                        }}
-                                        onClick={(e) => {
-                                            e.currentTarget.value = '';
-                                        }}
-                                    />
-                                </label>
-                            </li>
-                            <li>
-                                <label style={{ display: 'flex', alignItems: 'center' }} className="dropdown__link">
-                                    Load Template
-                                    <input
-                                        style={{ display: 'none' }}
-                                        type="file"
-                                        accept="application/json"
-                                        onChange={(e) => {
-                                            if (e.target && e.target.files) {
-                                                loadTemplate(e.target.files[0]);
-                                            }
-                                        }}
-                                        onClick={(e) => {
-                                            e.currentTarget.value = '';
-                                        }}
-                                    />
-                                </label>
-                            </li>
-                            <li>
-                                <div
-                                    style={{ display: 'flex', alignItems: 'center' }}
-                                    onClick={downloadTemplate}
-                                    className="dropdown__link"
-                                >
-                                    Download Template
-                                </div>
-                            </li>
-                            <li>
-                                <div
-                                    style={{ display: 'flex', alignItems: 'center' }}
-                                    onClick={generatePdf}
-                                    className="dropdown__link"
-                                >
-                                    Preview PDF
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                ) : (
-                    <div style={{ display: 'flex' }}>
-                        <label
-                            style={{ marginRight: '1rem', display: 'flex', alignItems: 'center' }}
-                            className="button button--sm button--outline button--success"
-                        >
-                            Change BasePDF
-                            <input
-                                style={{ display: 'none' }}
-                                type="file"
-                                accept="application/pdf"
-                                onChange={(e) => {
-                                    if (e.target && e.target.files) {
-                                        changeBasePdf(e.target.files[0]);
-                                    }
-                                }}
-                                onClick={(e) => {
-                                    e.currentTarget.value = '';
-                                }}
-                            />
-                        </label>
-
-                        <label
-                            style={{ marginRight: '1rem', display: 'flex', alignItems: 'center' }}
-                            className="button button--sm button--outline button--info"
-                        >
-                            Load Template
-                            <input
-                                style={{ display: 'none' }}
-                                type="file"
-                                accept="application/json"
-                                onChange={(e) => {
-                                    if (e.target && e.target.files) {
-                                        loadTemplate(e.target.files[0]);
-                                    }
-                                }}
-                                onClick={(e) => {
-                                    e.currentTarget.value = '';
-                                }}
-                            />
-                        </label>
-
-                        <button
-                            style={{ marginRight: '1rem', display: 'flex', alignItems: 'center' }}
-                            onClick={downloadTemplate}
-                            className="button button--sm button--outline button--warning"
-                        >
-                            Download Template
-                        </button>
-                        <button
-                            style={{ display: 'flex', alignItems: 'center' }}
-                            onClick={generatePdf}
-                            className="button button--sm button--outline button--secondary"
-                        >
-                            Generate PDF
-                        </button>
-                    </div>
-                )}
-            </div>
-            <div
-                ref={designerRef}
-                style={{ width: '100%', height: `calc(100vh - ${headerHeight + controllerHeight}px)` }}
-            />
+            <h1 className="text-4xl font-bold text-center my-5">PDF generator page</h1>
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>File</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            <label className='w-full'>
+                                Change BasePDF
+                                <input
+                                    className='hidden'
+                                    type="file"
+                                    accept="application/pdf"
+                                    onChange={(e) => {
+                                        if (e.target && e.target.files) {
+                                            changeBasePdf(e.target.files[0]);
+                                        }
+                                    }}
+                                    onClick={(e) => {
+                                        e.currentTarget.value = '';
+                                    }}
+                                />
+                            </label>
+                            <MenubarShortcut>⌘T</MenubarShortcut>
+                        </MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem>
+                            <label className='w-full'>
+                                Load Template
+                                <input
+                                    className='hidden'
+                                    type="file"
+                                    accept="application/json"
+                                    onChange={(e) => {
+                                        if (e.target && e.target.files) {
+                                            loadTemplate(e.target.files[0]);
+                                        }
+                                    }}
+                                    onClick={(e) => {
+                                        e.currentTarget.value = '';
+                                    }} />
+                            </label>
+                        </MenubarItem>
+                        <MenubarItem onClick={downloadTemplate}>Download Template</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger onClick={generatePdf}>Generate PDF</MenubarTrigger>
+                </MenubarMenu>
+            </Menubar>
+            <div id="container" ref={designerRef} className='w-full min-h-screen h-full' />
         </>
     );
 };
 
-export default TemplateDesign;
